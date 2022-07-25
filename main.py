ab477b792bc68c82fe030963e6c12cd3d7f9d4ec
A-Lazar-A <fub.lazar@mail.ru>
2022-07-25 19:44:00 +0300

    fix bug with any bad found link

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ import requests
 import re
 from random import choice
 
+
 def href_wiki_not_jpg(href):
     return href and re.compile('wiki').search(href) and not re.compile('.jpg').search(href)
+
 
 def recursive_following(link, steps, main_url):
     if steps == 0:
@@ -22,10 +24,12 @@ def recursive_following(link, steps, main_url):
     html = BeautifulSoup(response.content, 'lxml')
 
     title = html.find(id='firstHeading')
-    links = html.find(id='bodyContent').find_all(href=href_wiki_not_jpg)
+    links = html.select('#mw-content-text')[0].find_all(href=href_wiki_not_jpg)
     print(f'{title.text}\n{link}\n')
-
-    recursive_following(main_url + choice(links)['href'], steps, main_url)
+    try:
+        recursive_following(main_url + choice(links)['href'], steps, main_url)
+    except:
+        recursive_following(main_url + choice(links)['href'], steps, main_url)
 
 
 @click.command('Wiki Path')
@@ -37,17 +41,19 @@ def recursive_following(link, steps, main_url):
 @click.option('--steps', '-s', type=int, default=10, help='Choose number os steps')
 def wiki_path(request, language, steps):
     """
-    This is the program that shows to you what way you can go from provided article to another only with links in the
-    articles
-
+    This is the program that shows one a path made in required amount of steps from the provided Wikipedia article
+    to a random one
     """
     print(text2art('Wiki Path'))
     url = 'https://en.wikipedia.org/wiki/'
 
     if language == 'ru':
         url = 'https://ru.wikipedia.org/wiki/'
+    try:
+        recursive_following(url + request, steps, url[:-6])
+    except:
+        print('OOOPS, try again')
 
-    recursive_following(url + request, steps, url[:-6])
 
 
 if __name__ == '__main__':
