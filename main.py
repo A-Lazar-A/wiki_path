476b93195924dba7edf8e1d78d04b7bbc90c9a06
A-Lazar-A <fub.lazar@mail.ru>
2022-07-25 19:16:01 +0300

    fix .jpg bug

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import requests
 import re
 from random import choice
 
+def href_wiki_not_jpg(href):
+    return href and re.compile('wiki').search(href) and not re.compile('.jpg').search(href)
 
 def recursive_following(link, steps, main_url):
     if steps == 0:
@@ -20,7 +22,7 @@ def recursive_following(link, steps, main_url):
     html = BeautifulSoup(response.content, 'lxml')
 
     title = html.find(id='firstHeading')
-    links = html.find(id='bodyContent').find_all(href=re.compile('wiki'))
+    links = html.find(id='bodyContent').find_all(href=href_wiki_not_jpg)
     print(f'{title.text}\n{link}\n')
 
     recursive_following(main_url + choice(links)['href'], steps, main_url)
@@ -30,16 +32,14 @@ def recursive_following(link, steps, main_url):
 @click.argument('request')
 @click.option('--language', '-l',
               type=click.Choice(['eng', 'ru']),
-              help='Choose wiki language (ru and eng are required)',
+              help='Choose wiki language',
               default='eng')
 @click.option('--steps', '-s', type=int, default=10, help='Choose number os steps')
 def wiki_path(request, language, steps):
     """
     This is the program that shows to you what way you can go from provided article to another only with links in the
     articles
-    :param request: Wiki article
-    :param language: Provided language (ru or eng)
-    :param steps: How many steps do you want to go
+
     """
     print(text2art('Wiki Path'))
     url = 'https://en.wikipedia.org/wiki/'
